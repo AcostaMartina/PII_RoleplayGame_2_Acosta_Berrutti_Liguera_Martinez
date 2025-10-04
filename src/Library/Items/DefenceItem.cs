9d3241dab8680b55611a28eb9bf2bd3c7829524a
AcostaMartina <martina.acosta1605@gmail.com>
2025-10-04 16:40:25 -0300

    aaa

---
+++ src/Library/Items/DefenceItem.cs
@@ -0,0 +1,30 @@
+namespace Ucu.Poo.RoleplayGame;
+
+public class DefenceItem : IItem
+{
+    private string _name;
+    private int _armor;
+    private bool _isMagical;
+
+    public DefenceItem(string name, int armor, bool isMagical)
+    {
+        _name = name;
+        _armor = armor;
+        _isMagical = isMagical;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public bool IsMagical
+    {
+        get { return _isMagical; }
+    }
+
+    public int Armor
+    {
+        get { return _armor; }
+    }
+}
